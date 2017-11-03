56abf480f8dc6ec007887a2eb1d1360f7ef7a53e
reng <1837895991@qq.com>
2017-11-03 15:02:00 +0800

    add less into

---
+++ src/components/common/header.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="header">
+    this is the header.
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'header',
+    data () {
+      return {
+
+      }
+    }
+  }
+</script>
+
+<style scoped>
+  .header{
+    width:100%;
+    height:10px;
+    background:red;
+  }
+</style>
---
+++ src/components/index.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="hello">
+    <h1>{{ msg }}</h1>
+    <h2>Essential Links <em>reng</em></h2>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  data () {
+    return {
+      msg: 'Welcome to Your Vue.js App'
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="less">
+  .hello{
+    background:blue;
+    h2{
+      color:red;
+      em{
+        color:#fff;
+      }
+    }
+  }
+</style>
